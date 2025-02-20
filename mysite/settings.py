0f99390b898c3c21c673ae4b50ca1421b86861ec
johicasado <46503796+johicasado@users.noreply.github.com>
2025-02-20 19:05:59 -0300

    Update settings.py
    cambios para produccion

--- mysite/settings.py
+++ mysite/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x^&&ev38lwh(!!edu*=zgxbwz)=vgj)+ei!eog7fbn4m)q)x(b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
